03ff9d4629f70ab2143061b259e932fee9a5f120
imnulhaqueruman <imnulhaquerumantalukder1999@gmail.com>
2021-04-20 19:45:57 +0600

    animation

--- src/Components/Home/ServiceDetail/ServiceDetail.js
+++ src/Components/Home/ServiceDetail/ServiceDetail.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, useHistory } from 'react-router-dom';
+import {  useHistory } from 'react-router-dom';
 import './ServiceDetail.css';
 const ServiceDetail = ({data}) => {
     const history = useHistory()
